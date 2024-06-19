1447c567a70ff6b529ee9ecdc1df92ad63a92267
ygallardo2024 <y.gallardo@duocuc.cl>
2024-06-19 15:41:52 -0400

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    op=int(input("selecciona la operacion que deseas realizar\n1.-Suma\n2.-Resta\n3.-Multiplicar\n4.-Dividir\n"))
+    if op == 1 :
+        print ("seleccionaste sumar")
+
+
+
+        
+
+    elif op == 2 :
+        print ("seleccionaste restar")
+
+
+
+        
+    elif op == 3 :
+        print ("seleccionaste multiplicar")
+
+
+
+        
+    else:  
+        print ("seleccionaste dividir")
+
+
+
+        
+        
+ 
